a10e5749403ec2a02a6b987e0cf1de1720f18d05
Edward Weng <edweng@gmail.com>
2015-02-28 14:07:27 -0500

    Rescuing bad pings in ping_bot.

--- app/models/ping_bot.rb
+++ app/models/ping_bot.rb
@@ -1,9 +1,14 @@
 class PingBot < ActiveRecord::Base
   def self.run
     Destination.all.each do |destination|
-      response = HTTParty.get(destination.url)
-      if response.success?
-        destination.update_attributes(:last_ping => Time.now)
+      Rails.logger.info "NOW PINGING: #{destination.url}"
+      begin
+        response = HTTParty.get(destination.url)
+        if response.success?
+          destination.update_attributes(:last_ping => Time.now)
+        end
+      rescue Errno::ECONNREFUSED => e
+        Rails.logger.info "ERROR: #{e} - #{destination.url}"
       end
     end
   end
