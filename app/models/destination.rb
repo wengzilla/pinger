02661325781502aff01c9852fde487f104cc1b68
Edward Weng <edweng@gmail.com>
2015-03-08 21:41:33 -0400

    Updating all gems. Moving to Ruby 2.1.2

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,13 @@
 class Destination < ActiveRecord::Base
+  def ping
+    Rails.logger.info "NOW PINGING: #{url}"
+    begin
+      response = HTTParty.get(url)
+      if response.success?
+        update_attributes(:last_ping => Time.now)
+      end
+    rescue Errno::ECONNREFUSED => e
+      Rails.logger.info "ERROR: #{e} - #{url}"
+    end
+  end
 end
--- app/models/ping_bot.rb
+++ app/models/ping_bot.rb
@@ -1,15 +1,5 @@
 class PingBot < ActiveRecord::Base
   def self.run
-    Destination.all.each do |destination|
-      Rails.logger.info "NOW PINGING: #{destination.url}"
-      begin
-        response = HTTParty.get(destination.url)
-        if response.success?
-          destination.update_attributes(:last_ping => Time.now)
-        end
-      rescue Errno::ECONNREFUSED => e
-        Rails.logger.info "ERROR: #{e} - #{destination.url}"
-      end
-    end
+    Destination.all.each { |destination| destination.ping }
   end
 end
