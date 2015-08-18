47114d558ecf1b9399c5f19f511196ead8836843
Edward Weng <edweng@gmail.com>
2015-08-18 12:49:24 -0700

    Updating spring and restricting pings to happen between 1am and 9am EST

--- app/models/ping_bot.rb
+++ app/models/ping_bot.rb
@@ -1,5 +1,8 @@
 class PingBot < ActiveRecord::Base
   def self.run
-    Destination.all.each { |destination| destination.ping }
+    # if after 9am EST and before 1am EST 
+    if Time.now.hour > 13 || Time.now.hour < 5
+      Destination.all.each { |destination| destination.ping }
+    end
   end
 end
